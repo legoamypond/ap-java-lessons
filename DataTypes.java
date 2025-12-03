4dff8c826e9fbcfcaa717df3398ae0eb1610a25f
legoamypond <legoamypond@gmail.com>
2025-12-03 13:28:45 +0000

    FirstLessonDataType

---
+++ DataTypes.java
@@ -0,0 +1,18 @@
+//Java is a strongly typed language; When you define/declare a varible you have to define a data type
+class DataTypeExamples {
+    
+    int myInt = 42;
+    double myDouble = 189.75;
+    char myChar = 'Q';
+    boolean myBool = false;
+}
+
+public class DataTypes {
+    public static void main(String[]args){
+
+            DataTypeExamples dt = new DataTypeExamples();
+
+            System.out.println("myInt " + dt.myInt);
+
+    }   
+}

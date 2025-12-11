d75c175ce8e912b958218aad674fa605877bf308
legoamypond <legoamypond@gmail.com>
2025-12-11 15:36:14 +0000

    FinishedInputChallenge1&2

--- UserInput.java
+++ UserInput.java
@@ -17,5 +17,49 @@ public class UserInput {
     System.out.println("What is your height in meters");
     double height = Double.parseDouble(input.nextLine());
     System.out.println("You are " + height + " meters tall.");
-    }
+        
+    System.out.println("Enter name1: ");
+    String name1 = input.nextLine();
+    
+    System.out.println("Enter name2: ");
+    String name2 = input.nextLine();
+
+    System.out.println("Enter num1");
+    int num1 = Integer.parseInt(input.nextLine());
+    
+    System.out.println("Enter num2");
+    int num2 = Integer.parseInt(input.nextLine());
+    int newNum = 0;
+    if(num1+num2<100){
+            newNum = num1+num2;
+        }
+    else{
+            newNum = num1-num2;
+        } 
+
+
+    System.out.println(name1+name2+newNum+"@gmail.com");
+
+    System.out.print("Enter Decimal1 ");
+    double double1 = Double.parseDouble(input.nextLine()); 
+    
+    System.out.print("Enter Decimal2 ");
+    double double2 = Double.parseDouble(input.nextLine());
+
+    double q = double1/double2;
+    System.out.println(q);
+
+    System.out.println("Enter a diameter as a double ");
+    double diameter = Double.parseDouble(input.nextLine());
+    double radius = diameter/2;
+     
+
+
+
+}
+
+
+
+    
+
 }

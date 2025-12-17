ce54f7e06c2e6dbf870faeb8f91a26c289511f31
legoamypond <legoamypond@gmail.com>
2025-12-17 20:59:53 +0000

    startedLoopsandConditionals

---
+++ loops.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+public class loops {
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+         // break
+        // continue
+        // return
+        /* 
+        for (int i = 0; i < 5; i += 1) {
+            for (int j = 0; j < 5; j++) {
+                
+                    System.out.print(j);
+            }
+            System.out.print('\n');
+        }
+        // for (int i = 0; i < 5 || j == 5; i++)
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+        boolean isContinuing = true;
+        int z = 0;
+        while (isContinuing) {
+            z++;
+            if (z == 20) {
+                isContinuing = false;
+            }
+            System.out.println(z);
+        }
+                */
+    
+    /*for (int i = 0; i<100; i++){
+        if(i % 5 == 0){
+            if(i % 3 == 0){
+             System.out.println("Foobar");
+            }
+            else{
+                System.out.println("Foo");
+            }
+        }
+        else{
+            if(i % 3 == 0){
+                System.out.println("bar");
+            }
+        }
+        }  */
+
+    Boolean isWon = false; 
+    System.out.println("Enter a number: 1-3");
+    while(isWon){
+        int num = input.nextLine();
+        
+    } 
+       
+}
+}
+
+

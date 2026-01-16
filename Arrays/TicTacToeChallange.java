e9348c20e39e67b1b963c86fb11f0c3e6885a866
legoamypond <legoamypond@gmail.com>
2026-01-16 21:06:22 +0000

    ticTacToeErrorsChallange1Fixed

--- Arrays/TicTacToeChallange.java
+++ Arrays/TicTacToeChallange.java
@@ -4,14 +4,16 @@ public class TicTacToeChallange {
         Random random = new Random();
         String[][] ticTacBoard = new String[3][3]; 
         int xCount = 0;
+        int oCount = 0; 
         for(int i =  0; i < ticTacBoard.length; i++){
             System.out.println();
             for(int j = 0; j < ticTacBoard[i].length; j++){
                 int nextMarkInt = random.nextInt(2);
                 String nextMark = "o";
                 if(nextMarkInt == 1){
-                    if(xCount == 4){
+                    if(xCount == 5){
                         nextMark = "o";
+                        oCount++;
                     }
                     else{
                         nextMark = "x";
@@ -19,11 +21,31 @@ public class TicTacToeChallange {
                     }
                 }
                 else{
-                    nextMark = "o"; 
+                    nextMark = "o";
+                    oCount++; 
                 }
                 ticTacBoard[i][j] = nextMark;
                 System.out.print(ticTacBoard[i][j]+" ");
+         }
         }
+       while(oCount >= 6){
+        while(true){
+            int row = random.nextInt(4);
+            int collum = random.nextInt(4); 
+            if(ticTacBoard[row][collum].contentEquals("o")){
+                ticTacBoard[row][collum] = "x";
+                oCount -= 1;
+            }
         }
+       }
+        /*//Rows
+        for(int row = 0; row<3; row++){
+            System.out.println();
+            System.out.println(ticTacBoard[row][row]);
+            if(ticTacBoard[row][row] == "x"){
+                System.out.println("Yep");
+            }
+    
+        }*/
     }
 }
